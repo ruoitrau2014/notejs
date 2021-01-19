0979467935ae3289425ac07070409c5a451aa5cb
ruoitrau2014 <nganv1986@gmail.com>
2021-01-19 21:01:10 +0700

    Validate các biến môi trường

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 import { IndexRoute } from './modules/index';
 import App from './app';
+import { validateEnv } from './core/utils';
 const routes=[new IndexRoute()];
 const app=new App(routes);
+validateEnv();
 app.listen();
 console.log("hello");
